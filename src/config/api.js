9209697038c73495820881c7827ef717ab213b5c
sandriansyah <sandriansyah38@gmail.com>
2022-04-03 09:17:41 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const API = axios.create({
-    baseURL: "https://wow-app-server.herokuapp.com/",
+    baseURL: "https://wow-app-server.herokuapp.com/api/v1",
 });
 
 export const setAuthToken = (token) => {
