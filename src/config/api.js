e9bdd756db3eee9bb334a8d9b0c47e486b3b3677
sandriansyah <sandriansyah38@gmail.com>
2022-04-12 23:43:04 +0700

    update

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 export const API = axios.create({
     baseURL: 
-    process.env.SERVER.URL ||
     "https://dashboard.heroku.com/apps/wow-app-server/api/v1" ||
     "http://localhost:5000/api/v1",
 });
